6c186fffad95b2f2b6bf8d10677cd305b3db3fde
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-05-08 17:55:45 +0200

    Add proper project skeleton

---
+++ README.md
@@ -0,0 +1,27 @@
+# [WIP] Elixir GCP Speech-to-Text API client
+
+Elixir client for Google Cloud Speech-to-Text API using gRPC
+
+## Installation
+
+When published, the package can be installed by adding `elixir_gcp_speech` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:elixir_gcp_speech, "~> 0.1.0"}
+  ]
+end
+```
+
+## Sponsors
+
+This project is sponsored by [Abridge AI, Inc.](https://abridge.ai)
+
+## Copyright and License
+
+Copyright 2019, [Software Mansion](https://swmansion.com)
+
+[![Software Mansion](https://membraneframework.github.io/static/logo/swm_logo_readme.png)](https://swmansion.com)
+
+Licensed under the [Apache License, Version 2.0](LICENSE)
--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
-defmodule GCloudSpeechGrpc.MixProject do
+defmodule ExGCloudSpeech.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/SoftwareMansion/elixir-gcp-speech"
 
   def project do
     [
-      app: :gcloud_speech_grpc,
-      version: "0.1.0",
+      app: :elixir_gcp_speech,
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      description: "Elixir client for Google Cloud Speech-to-Text API using gRPC",
+      package: package(),
+
+      # docs
+      name: "Elixir GCP Speech",
+      source_url: @github_url,
+      docs: docs()
     ]
   end
 
@@ -19,11 +31,28 @@ defmodule GCloudSpeechGrpc.MixProject do
 
   defp deps do
     [
-    {:protobuf, "~> 0.5.3"},
-    # {:grpc, github: "elixir-grpc/grpc"}
-    # Only for files generated from Google's protos.
-    # Can be ignored if you don't use Google's protos.
-    #{:google_protos, "~> 0.1"}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:goth, "~> 1.0"},
+      {:protobuf, "~> 0.6.1"},
+      {:grpc, "~> 0.3.1"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Bartosz Błaszków"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}"
     ]
   end
 end
