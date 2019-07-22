e8033709b0d5a49c6080ae4ed042669b259def45
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-07-22 14:04:05 +0200

    Update googleapis

--- lib/google/cloud/speech/v1/cloud_speech.pb.ex
+++ lib/google/cloud/speech/v1/cloud_speech.pb.ex
@@ -71,6 +71,7 @@ defmodule Google.Cloud.Speech.V1.RecognitionConfig do
           speech_contexts: [Google.Cloud.Speech.V1.SpeechContext.t()],
           enable_word_time_offsets: boolean,
           enable_automatic_punctuation: boolean,
+          metadata: Google.Cloud.Speech.V1.RecognitionMetadata.t() | nil,
           model: String.t(),
           use_enhanced: boolean
         }
@@ -85,6 +86,7 @@ defmodule Google.Cloud.Speech.V1.RecognitionConfig do
     :speech_contexts,
     :enable_word_time_offsets,
     :enable_automatic_punctuation,
+    :metadata,
     :model,
     :use_enhanced
   ]
@@ -99,6 +101,7 @@ defmodule Google.Cloud.Speech.V1.RecognitionConfig do
   field :speech_contexts, 6, repeated: true, type: Google.Cloud.Speech.V1.SpeechContext
   field :enable_word_time_offsets, 8, type: :bool
   field :enable_automatic_punctuation, 11, type: :bool
+  field :metadata, 9, type: Google.Cloud.Speech.V1.RecognitionMetadata
   field :model, 13, type: :string
   field :use_enhanced, 14, type: :bool
 end
@@ -115,6 +118,101 @@ defmodule Google.Cloud.Speech.V1.RecognitionConfig.AudioEncoding do
   field :AMR_WB, 5
   field :OGG_OPUS, 6
   field :SPEEX_WITH_HEADER_BYTE, 7
+end
+
+defmodule Google.Cloud.Speech.V1.RecognitionMetadata do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          interaction_type: atom | integer,
+          industry_naics_code_of_audio: non_neg_integer,
+          microphone_distance: atom | integer,
+          original_media_type: atom | integer,
+          recording_device_type: atom | integer,
+          recording_device_name: String.t(),
+          original_mime_type: String.t(),
+          audio_topic: String.t()
+        }
+  defstruct [
+    :interaction_type,
+    :industry_naics_code_of_audio,
+    :microphone_distance,
+    :original_media_type,
+    :recording_device_type,
+    :recording_device_name,
+    :original_mime_type,
+    :audio_topic
+  ]
+
+  field :interaction_type, 1,
+    type: Google.Cloud.Speech.V1.RecognitionMetadata.InteractionType,
+    enum: true
+
+  field :industry_naics_code_of_audio, 3, type: :uint32
+
+  field :microphone_distance, 4,
+    type: Google.Cloud.Speech.V1.RecognitionMetadata.MicrophoneDistance,
+    enum: true
+
+  field :original_media_type, 5,
+    type: Google.Cloud.Speech.V1.RecognitionMetadata.OriginalMediaType,
+    enum: true
+
+  field :recording_device_type, 6,
+    type: Google.Cloud.Speech.V1.RecognitionMetadata.RecordingDeviceType,
+    enum: true
+
+  field :recording_device_name, 7, type: :string
+  field :original_mime_type, 8, type: :string
+  field :audio_topic, 10, type: :string
+end
+
+defmodule Google.Cloud.Speech.V1.RecognitionMetadata.InteractionType do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+
+  field :INTERACTION_TYPE_UNSPECIFIED, 0
+  field :DISCUSSION, 1
+  field :PRESENTATION, 2
+  field :PHONE_CALL, 3
+  field :VOICEMAIL, 4
+  field :PROFESSIONALLY_PRODUCED, 5
+  field :VOICE_SEARCH, 6
+  field :VOICE_COMMAND, 7
+  field :DICTATION, 8
+end
+
+defmodule Google.Cloud.Speech.V1.RecognitionMetadata.MicrophoneDistance do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+
+  field :MICROPHONE_DISTANCE_UNSPECIFIED, 0
+  field :NEARFIELD, 1
+  field :MIDFIELD, 2
+  field :FARFIELD, 3
+end
+
+defmodule Google.Cloud.Speech.V1.RecognitionMetadata.OriginalMediaType do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+
+  field :ORIGINAL_MEDIA_TYPE_UNSPECIFIED, 0
+  field :AUDIO, 1
+  field :VIDEO, 2
+end
+
+defmodule Google.Cloud.Speech.V1.RecognitionMetadata.RecordingDeviceType do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+
+  field :RECORDING_DEVICE_TYPE_UNSPECIFIED, 0
+  field :SMARTPHONE, 1
+  field :PC, 2
+  field :PHONE_LINE, 3
+  field :VEHICLE, 4
+  field :OTHER_OUTDOOR_DEVICE, 5
+  field :OTHER_INDOOR_DEVICE, 6
 end
 
 defmodule Google.Cloud.Speech.V1.SpeechContext do
@@ -218,9 +316,11 @@ defmodule Google.Cloud.Speech.V1.StreamingRecognitionResult do
           alternatives: [Google.Cloud.Speech.V1.SpeechRecognitionAlternative.t()],
           is_final: boolean,
           stability: float,
-          channel_tag: integer
+          result_end_time: Google.Protobuf.Duration.t() | nil,
+          channel_tag: integer,
+          language_code: String.t()
         }
-  defstruct [:alternatives, :is_final, :stability, :channel_tag]
+  defstruct [:alternatives, :is_final, :stability, :result_end_time, :channel_tag, :language_code]
 
   field :alternatives, 1,
     repeated: true,
@@ -228,7 +328,9 @@ defmodule Google.Cloud.Speech.V1.StreamingRecognitionResult do
 
   field :is_final, 2, type: :bool
   field :stability, 3, type: :float
+  field :result_end_time, 4, type: Google.Protobuf.Duration
   field :channel_tag, 5, type: :int32
+  field :language_code, 6, type: :string
 end
 
 defmodule Google.Cloud.Speech.V1.SpeechRecognitionResult do
