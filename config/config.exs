ec7d9bb057d8eb0bc600c9385a5ad364a2e8ca8c
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-08-22 16:35:33 +0200

    Add CircleCI config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
-config :goth, json: Path.expand("./creds.json", __DIR__) |> File.read!()
+creds_path = Path.expand("./creds.json", __DIR__)
+
+if creds_path |> File.exists?() do
+  config :goth, json: creds_path |> File.read!()
+else
+  config :goth, disabled: true
+end
