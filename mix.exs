71b38311c3932a23cbaada3a8f6ed69c165792f2
Deric Walintukan <deric@loopapps.com>
2023-10-23 16:05:47 -0700

    downgrade grpc to 0.5.0

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule GCloud.SpeechAPI.MixProject do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:goth, "~> 1.0"},
       {:protobuf, "~> 0.6.1"},
-      {:grpc, "~> 0.7.0"},
+      {:grpc, "~> 0.5.0"},
       {:certifi, "~> 2.5"}
     ]
   end
